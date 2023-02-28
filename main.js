43475f19a7d46644b2724ac4cbec88c869b6a1be
Jared Smith <50846985+realbearsmith@users.noreply.github.com>
2023-02-27 21:21:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -17,13 +17,15 @@ const dbx = new Dropbox({
 });
 
 // Use the object to get a new access token
-const oauth2Api = new Dropbox.DropboxOAuth2Api(dbx);
-oauth2Api.tokenFromOAuth1(REFRESH_TOKEN).then((accessToken) => {
-  dbx.setAccessToken(accessToken.result.access_token);
-  console.log('Access token:', accessToken.result.access_token);
-}).catch((error) => {
-  console.error(error);
-});
+dbx.auth
+  .tokenFromOAuth1({ oauth1_token_secret: REFRESH_TOKEN })
+  .then((response) => {
+    dbx.setAccessToken(response.result.access_token);
+    console.log('Access token:', response.result.access_token);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // Get NYT Crossword
 function getNYTC(date) {
